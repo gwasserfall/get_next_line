4a8df4e65747997ecacbdb41d15582cd2e346199
Glen WASSERFALL <glen@wasserfalls.co.za>
2019-06-29 12:06:14 +0200

    Removed unused source headers

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,16 @@
 /*   By: gwasserf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 11:34:16 by gwasserf          #+#    #+#             */
-/*   Updated: 2019/06/29 11:44:18 by gwasserf         ###   ########.fr       */
+/*   Updated: 2019/06/29 12:05:44 by gwasserf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <ctype.h>
+# include "stdlib.h"
 # include "libft/libft.h"
 
 # define BUFF_SIZE 42
