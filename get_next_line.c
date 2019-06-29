7940da0fd3b777007dc924bbc83f551b48046e27
Glen WASSERFALL <glen@wasserfalls.co.za>
2019-06-29 11:52:54 +0200

    Finalizing

--- get_next_line.c
+++ get_next_line.c
@@ -1,25 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwasserf <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/29 11:34:23 by gwasserf          #+#    #+#             */
+/*   Updated: 2019/06/29 11:42:07 by gwasserf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 t_list	*ft_get_fdlist(int fd, t_list **alst)
 {
-    t_list *head;
-    
-    if (!(*alst))
-    {
-        *alst = ft_lstnew("\0", fd);
-        (*alst)->content_size = fd;
-    }
-    head = *alst; 
-    while (head)
-    {
-        if ((int)head->content_size == fd)
-            return head;
-        head = head->next;
-    }
-    head = ft_lstnew("\0", 1);
-    head->content_size = fd;
-    ft_lstadd(alst, head);
-    return (head);
+	t_list *head;
+
+	if (!(*alst))
+		*alst = ft_lstnew("\0", fd);
+	head = *alst;
+	while (head)
+	{
+		if ((int)head->content_size == fd)
+			return (head);
+		head = head->next;
+	}
+	head = ft_lstnew("\0", fd);
+	ft_lstadd(alst, head);
+	return (head);
 }
 
 int		fetch_line(char **store, char **line)
@@ -29,18 +37,18 @@ int		fetch_line(char **store, char **line)
 	char	*end;
 
 	if ((*store)[0] == 4)
-		return 0;
+		return (0);
 	if ((end = ft_strchr(*store, 4)))
 		if (!(ft_strchr(*store, '\n')))
 			*end = '\n';
 	temp = ft_strdup(*store);
 	ptr = ft_strchr(temp, '\n');
-	*ptr = 0; 
+	*ptr = 0;
 	*line = ft_strdup(temp);
 	free(*store);
 	*store = ft_strdup((ptr + 1));
 	free(temp);
-	return 1;
+	return (1);
 }
 
 void	read_until_line(int fd, char **store)
@@ -48,29 +56,29 @@ void	read_until_line(int fd, char **store)
 	char	buffer[BUFF_SIZE + 2];
 	char	*temp;
 	int		ret;
-  
+
 	while (!ft_strchr(*store, '\n'))
 	{
-	ret = read(fd, buffer, BUFF_SIZE);
-	buffer[ret] = (ret < BUFF_SIZE) ? 4 : 0;
-	buffer[ret + 1] = 0;
-	temp = ft_strdup(*store);
-	free(*store);
-	*store = ft_strjoin(temp, buffer);
-	free(temp);
-	if (!ret)
-		return ;
+		ret = read(fd, buffer, BUFF_SIZE);
+		buffer[ret] = (ret < BUFF_SIZE) ? 4 : 0;
+		buffer[ret + 1] = 0;
+		temp = ft_strdup(*store);
+		free(*store);
+		*store = ft_strjoin(temp, buffer);
+		free(temp);
+		if (!ret)
+			return ;
 	}
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static t_list *list;
+	static t_list	*list;
+	t_list			*current;
 
-	t_list *current;
 	if (fd < 0 || !line || (read(fd, 0, 0) < 0))
 		return (-1);
 	current = ft_get_fdlist(fd, &list);
 	read_until_line(fd, (char **)&current->content);
-	return fetch_line((char **)&current->content, line);
+	return (fetch_line((char **)&current->content, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwasserf <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/29 11:34:16 by gwasserf          #+#    #+#             */
+/*   Updated: 2019/06/29 11:44:18 by gwasserf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
@@ -7,9 +19,10 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <ctype.h>
-# include "libft/includes/libft.h"
+# include "libft/libft.h"
 
-# define BUFF_SIZE 2
+# define BUFF_SIZE 42
 
 int get_next_line(const int fd, char **line);
+
 #endif
